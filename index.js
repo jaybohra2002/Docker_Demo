d0baf3213a0a4884476cb0d0ff45e09c20061e8a
jaybohra2002 <jaybohra380@gmail.com>
2024-07-29 21:16:29 +0530

    Basic changes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express=require('express');
 const app=express();
+const dotenv=require('dotenv');
+dotenv.config();
 app.use('/home',(req,res)=>{
     res.json({message:"OK"});
 })
-app.listen(3002,()=>{
-    console.log('server is running on port 3002');
+app.listen(process.env.PORT,()=>{
+    console.log(`server is ruinng on port: ${process.env.PORT}`);
 })
